35d6804cce836895c777e37983b01a60611eb055
Taya <taya@hottea.jp>
2013-02-17 02:19:46 +0900

    Initial Commit

---
+++ shaderTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  shaderTest
+//
+//  Created by Shintaro Taya on 13/02/17.
+//  Copyright taoru 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
